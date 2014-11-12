c684f300d58014d24f1bb824977e960352ea51c8
Justin Murphy <j.murphy@soton.ac.uk>
2014-11-12 15:06:39 +0000

    regional graphs

---
+++ content/pacification_of_the_world.knit.md
@@ -0,0 +1,39 @@
+---
+title: "The Pacification of the World: Notes on the Politics of Information and Communication"
+author: "Justin Murphy"
+date: 
+output:
+  pdf_document:
+    fig_caption: yes
+    includes:
+      before_body: prefix.tex
+      in_header: mystyles.sty
+    keep_tex: yes
+    latex_engine: xelatex
+    toc: yes
+  html_document:
+    keep_md: yes
+    theme: readable
+    toc: yes
+    toc_depth: 3
+citecolor: red
+fontsize: 12pt
+geometry: margin=1.5in
+linkcolor: red
+mainfont: PT Serif
+classoption: book
+bibliography: mybib.bib
+urlcolor: red
+---
+
+
+
+
+
+# Part 1: General Theoretical Reflections
+
+
+
+
+
+
--- data/banks_2014/cleaned/clean.R
+++ data/banks_2014/cleaned/clean.R
@@ -2,8 +2,9 @@ require(gdata)
 require(reshape2)
 require(ggplot2)
 require(zoo)
+require(WDI)
 
-setwd("~/Dropbox/Data General/banks_2014")
+setwd("~/Dropbox/gh_projects/pacification_of_the_world/data/banks_2014")
 
 df<-read.xls("CNTSDATA.xls")
 df<-subset(df, code!="16524")
@@ -21,11 +22,22 @@ df$computers<-df$computer6*.0001  # PCs per capita
 df$radios<-df$media2*.0001
 df$tvs<-df$media4*.00001
 df$newspapers<-df$media5*.0001
+df$mdi<-rowMeans(subset(df, select=c("tvs", "newspapers", "radios"), na.rm=FALSE))*100 
 
 df$landlines<-df$phone4*.00001
 df$mobiles<-df$phone5*.00001
 
 df$industry<-df$industry4*.1 #percent working in industry
+
+wdi<-WDI(country = "all", extra=TRUE)
+wdi$Wbcode<-wdi$iso3c
+
+df<-merge(df, wdi[c("Wbcode", "region")],  by=c("Wbcode"), all.x=TRUE)
+df<-unique(df)
+df<-df[with(df, order(country, year)), ]
+df$region<-gsub("\\(all income levels\\)", "", df$region)
+df$region[is.na(df$region)] <- "Other"
+df$region<-as.factor(df$region)
 
 df$oecd<-ifelse(df$country=="Austria" |
                   df$country=="Belgium" |
@@ -52,7 +64,7 @@ df$oecd<-ifelse(df$country=="Austria" |
 
 write.csv(subset(df, oecd=="OECD"), file="cleaned/oecd.csv")
 
-write.csv(subset(df, select=c("country", "year", "Wbcode", "oecd", "guerilla", "overall.conflict", "riots", "strikes", "demos", "assassinations",
-                              "internet", "computers", "radios", "tvs", "newspapers", "landlines", "mobiles")), file="cleaned/media_politics.csv")
+write.csv(subset(df, select=c("country", "year", "Wbcode", "region", "oecd", "guerilla", "overall.conflict", "riots", "strikes", "demos", "assassinations",
+                              "internet", "computers", "radios", "tvs", "newspapers", "mdi", "landlines", "mobiles")), file="cleaned/media_politics.csv")
 
 
